8525f7a92e12e9048aac8d68db4f3dc8c32d48ef
apollocked <hamabarznji1990@gmail.com>
2026-05-21 23:59:38 +0300

    Add: methods for crud

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:supabase/model/note.dart';
+import 'package:supabase/service/note_database.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final noteDatabase = NoteDatabase();
+
+  final noteControler = TextEditingController();
+
+  //methods
+  updateNote(Note note) {
+    noteDatabase.updateNote(note, noteControler.text);
+  }
+
+  deleteNoe(Note note) {
+    noteDatabase.deleteNote(note);
+  }
+
+  createNote() async {
+    final note = Note(content: noteControler.text);
+    await noteDatabase.insertNote(note);
+    noteControler.clear();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: noteControler,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter a note',
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
