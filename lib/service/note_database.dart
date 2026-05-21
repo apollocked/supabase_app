8d54c4b777326ed0f6698e2ba91f27e16a7fc6d0
apollocked <hamabarznji1990@gmail.com>
2026-05-21 23:51:50 +0300

    Add: mode and service for reading the data

---
+++ lib/service/note_database.dart
@@ -0,0 +1,23 @@
+import 'package:supabase/model/note.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class NoteDatabase {
+  final database = Supabase.instance.client.from('notes');
+
+  Future<void> insertNote(Note note) async {
+    await database.insert(note.toMap());
+  }
+
+  final stream = Supabase.instance.client
+      .from('notes')
+      .strem(primaryKey: ['id'])
+      .map((data) => data.map((noteMap) => Note.fromMap(noteMap)).toList());
+
+  Future<void> updateNote(Note note, String content) async {
+    await database.update({'content': content}).eq('id', note.id);
+  }
+
+  Future<void> deleteNote(Note note) async {
+    await database.delete().eq('id', note.id);
+  }
+}
